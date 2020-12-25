1d18d8f2cf5a7e9f561d6fb2bb13b3eeef734aec
Florence <monnaie.florence@gmail.com>
2020-12-25 22:07:14 +0100

    falling down effect done ! Yeaaah

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-// BEEHIVE rotate on click - start
 
 let hexa2 = document.getElementById('hexa2');
 let hexaH2 = document.getElementById('hexaH2');
@@ -8,6 +7,36 @@ let allHexa = document.getElementsByClassName('hexa');
 let allHexaH = document.getElementsByClassName('hexaH');
 let allHexaB = document.getElementsByClassName('hexaB');
 
+// Beehive Droping effect - start
+// on landing page
+document.addEventListener('DOMContentLoaded',function(){
+    // 1st TimeOut for delay on landing page
+    setTimeout(() => {
+        for(var i=0; i < allHexa.length; i++){ 
+            (function(i) {
+                // 2nd time out for each item (each i)
+               setTimeout(function(){
+                   allHexa[i].classList.add("falling-animation");
+                   allHexaB[i].classList.add("falling-animation");
+
+                // 3rd timeout for removing animation and resetting everyting into place
+                   setTimeout(() => {
+                       allHexa[i].classList.remove('falling-animation');
+                        allHexa[i].style.opacity = "1";
+                        allHexaB[i].classList.remove('falling-animation');
+                        allHexaB[i].style.opacity = "1";
+                   }, 1000);
+                }, i * 120);
+            })(i);
+        };
+    }, 1200);
+    
+});
+
+// Beehive Droping effect - end
+
+
+// BEEHIVE rotate on click - start
 for (let i = 0; i < allHexa.length; i++) {
     allHexaH[i].addEventListener('click',function(){
 
@@ -22,6 +51,13 @@ for (let i = 0; i < allHexa.length; i++) {
     allHexa[i].classList.toggle('transitionHexa');
     allHexaB[i].classList.toggle('transitionHexaB');
     }); 
+
+    // for falling down effect, opacity to zero to begin
+    allHexa[i].style.opacity = "0";
+    allHexaB[i].style.opacity = "0";
 };
+
+
+
 
 // BEEHIVE rotate on click - end
